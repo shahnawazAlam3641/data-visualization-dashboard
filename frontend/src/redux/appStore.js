3619a00dd3f27c3f6925c7b270fcb4039ea02e86
shahnawazAlam3641 <shahnawazalam3641@gmail.com>
2025-01-04 18:53:18 +0530

    add dataSetSlice

--- frontend/src/redux/appStore.js
+++ frontend/src/redux/appStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import dataSetReducer from "./dataSetSlice";
 
 const appStore = configureStore({
   reducer: {
     user: userReducer,
+    dataSet: dataSetReducer,
   },
 });
 
